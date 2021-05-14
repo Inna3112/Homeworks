fae5f1d620c3a5138d6d56316eefb92576d2d5a5
Inna3112 <innula3112@gmail.com>
2021-05-14 12:47:54 +0300

    Updates

---
+++ precache-manifest.563e243ccc5ddcab60480e10d60d04dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c6ef8de4528571b9930f46026b203e",
+    "url": "/Homeworks/index.html"
+  },
+  {
+    "revision": "845f7031201ae0bd3d82",
+    "url": "/Homeworks/static/css/main.9b80f8c1.chunk.css"
+  },
+  {
+    "revision": "3faa1f8a678db94d9c38",
+    "url": "/Homeworks/static/js/2.9cf31ddf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Homeworks/static/js/2.9cf31ddf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "845f7031201ae0bd3d82",
+    "url": "/Homeworks/static/js/main.99611f32.chunk.js"
+  },
+  {
+    "revision": "81af1bc03063c413e9cd",
+    "url": "/Homeworks/static/js/runtime-main.d775281f.js"
+  }
+]);
