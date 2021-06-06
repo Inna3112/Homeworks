3c803e62d13441c82ec0acce10ece32cab1f6614
Inna3112 <innula3112@gmail.com>
2021-06-06 13:44:00 +0300

    Added HW10

--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,29 @@
-const initState = {
 
+export type initStateType = typeof initState
+type ActionType = {
+    type: 'CHANGE-LOADING',
+    isLoading: boolean,
+}
+
+const initState = {
+    isLoading: false
 };
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: ActionType): initStateType => { // fix any
     switch (action.type) {
-        case "": {
-            return state;
+        case 'CHANGE-LOADING': {
+            return {
+                ...state,
+                isLoading: action.isLoading
+            }
         }
         default: return state;
     }
 };
 
-export const loadingAC = (): any => {}; // fix any
+export const loadingAC = (isLoading: boolean): ActionType => {
+    return {
+        type: 'CHANGE-LOADING',
+        isLoading
+    }
+}// fix any
